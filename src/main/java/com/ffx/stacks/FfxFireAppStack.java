169a60ca794e942c288bbeb01d964a83f3a0b43c
Louis <louis.drotos@gmail.com>
2024-05-25 15:00:34 -0400

    Added user data to EC2 service instance for start spring boot application as a linux service

--- src/main/java/com/ffx/stacks/FfxFireAppStack.java
+++ src/main/java/com/ffx/stacks/FfxFireAppStack.java
@@ -2,11 +2,14 @@ package com.ffx.stacks;
 
 import software.constructs.Construct;
 
+import java.util.List;
 import java.util.Map;
 
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.s3.Bucket;
+import software.amazon.awscdk.services.s3.deployment.BucketDeployment;
+import software.amazon.awscdk.services.s3.deployment.Source;
 import software.amazon.awscdk.services.ec2.AmazonLinuxGeneration;
 import software.amazon.awscdk.services.ec2.AmazonLinuxImage;
 import software.amazon.awscdk.services.ec2.Instance;
@@ -14,6 +17,7 @@ import software.amazon.awscdk.services.ec2.InstanceClass;
 import software.amazon.awscdk.services.ec2.InstanceSize;
 import software.amazon.awscdk.services.ec2.InstanceType;
 import software.amazon.awscdk.services.ec2.KeyPair;
+import software.amazon.awscdk.services.ec2.UserData;
 import software.amazon.awscdk.services.iam.ManagedPolicy;
 import software.amazon.awscdk.services.iam.Role;
 import software.amazon.awscdk.services.iam.ServicePrincipal;
@@ -35,6 +39,10 @@ public class FfxFireAppStack extends Stack {
 
         // Creates the staging bucket for storing artifacts that will be deployed
         Bucket bucket = new Bucket(this, projectName.concat("-staging"));
+        BucketDeployment.Builder.create(this, "DeployWebsite")
+            .sources(List.of(Source.asset("./assets")))
+            .destinationBucket(bucket)
+            .build();
 
         // Creates the IAM role that will give read access to the staging bucket 
         Role role = Role.Builder.create(this, projectName.concat("-services-role"))
@@ -44,6 +52,19 @@ public class FfxFireAppStack extends Stack {
             .build();
         role.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName("AmazonS3ReadOnlyAccess"));
 
+        // Creates the user data script for the EC2 instance
+        UserData userData = UserData.forLinux();
+        userData.addCommands("curl \"https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip\" -o \"awscliv2.zip\"");
+        userData.addCommands("unzip awscliv2.zip");
+        userData.addCommands("sudo ./aws/install");
+        userData.addCommands("sudo yum install java-17-amazon-corretto.x86_64 -y");
+        userData.addCommands("sudo aws s3 cp s3://ffxfireappstack-ffxfirestagingdcae2ed8-06mrmtpaxhu9/ffx-fire-app.service /etc/systemd/system/ffx-fire-app.service");
+        userData.addCommands("sudo aws s3 cp s3://ffxfireappstack-ffxfirestagingdcae2ed8-06mrmtpaxhu9/fire-services.jar /opt/fire-services.jar");
+        userData.addCommands("sudo aws s3 cp s3://ffxfireappstack-ffxfirestagingdcae2ed8-06mrmtpaxhu9/application.properties /opt/application.properties");
+        userData.addCommands("sudo systemctl daemon-reload");
+        userData.addCommands("sudo systemctl enable ffx-fire-app.service");
+        userData.addCommands("sudo systemctl start ffx-fire-app");
+
         // Creates the services instance
         Instance instance = Instance.Builder.create(this, projectName.concat("-services"))
             .vpc(network.getVpc())
@@ -52,6 +73,8 @@ public class FfxFireAppStack extends Stack {
             .machineImage(AmazonLinuxImage.Builder.create().generation(AmazonLinuxGeneration.AMAZON_LINUX_2023).build())
             .securityGroup(network.getServiceInstanceSecurityGroup())
             .keyPair(KeyPair.fromKeyPairName(this, keyPairId, keyPairName))
+            .role(role)
+            .userData(userData)
             .build();
         
     }
